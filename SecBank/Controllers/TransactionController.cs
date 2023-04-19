9a70049b77dd5dbe2c162651ce67bad020322155
victorpraiz <ivictorpraiz@gmail.com>
2023-04-19 10:11:58 +0100

    switched to sqlite

--- SecBank/Controllers/TransactionController.cs
+++ SecBank/Controllers/TransactionController.cs
@@ -73,7 +73,7 @@ namespace SecBank.Controllers
 
                     };
 
-                    break;
+                    
                 }
                 
             }
---
+++ SecBank/Migrations/20230419083136_switched to sqlite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecBank.Migrations
+{
+    /// <inheritdoc />
+    public partial class switchedtosqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<double>(type: "REAL", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    CreditCard = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsReccurent = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
