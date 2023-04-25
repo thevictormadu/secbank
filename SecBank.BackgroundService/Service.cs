4fb677c3925f41435eb797819f379f178a9cf10a
victorpraiz <ivictorpraiz@gmail.com>
2023-04-25 06:13:10 +0100

    logged windows service

---
+++ SecBank.BackgroundService/Service.cs
@@ -0,0 +1,41 @@
+﻿using SecBank.Entities.Models;
+
+namespace SecBank.BgService
+{
+    public class Service : IService
+    {
+        private readonly ServiceDbContext _db;
+
+        public Service(ServiceDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool PostTransaction()
+        {
+            bool success = false;
+            var transactionsFromDb = _db.Transactions.Where(t => t.IsReccurent == true).ToList();
+
+            foreach (var transaction in transactionsFromDb)
+            {
+                //if (transaction.DateCreated.AddDays(30) == DateTime.Now)
+                //{
+                    Transaction newTransaction = new Transaction()
+                    {
+                        Amount = transaction.Amount,
+                        Description = transaction.Description,
+                        DateCreated = DateTime.Now,
+                        CreditCard = transaction.CreditCard,
+                        IsReccurent = true
+                    };
+                    _db.Transactions.Add(newTransaction);
+                    transaction.IsReccurent = false;
+                    success = true;
+                //}
+            }
+
+            _db.SaveChanges();
+            return success;
+        }
+    }
+}
---
+++ SecBank.BackgroundService/ServiceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecBank.Entities.Models;
+
+namespace SecBank.BgService
+{
+    public class ServiceDbContext : DbContext
+    {
+        public ServiceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
